c7133f05f0aa8f7647e4c0a250c8562e7eaf64d6
Silvaire <105923294+Silvaternia@users.noreply.github.com>
2025-03-05 22:01:20 +0100

    Removed old project
    Wasn't motivated enough to rewrite htis crap, so I'll start from scratch

---
+++ SavourySlices/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5e*885_5s6rk$&a-9&t@y7*9!ji=94frpu&mr!2!bx&+k%mexj"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
--- js/data/updateStorage.js
+++
@@ -1,8 +0,0 @@
-import fetchProducts from './fetchProducts.js';
-import generateTable from '../components/tableGeneration.js';
-
-export default async function updateProductTable(products) {
-    const productsData = products || await fetchProducts();
-    await generateTable(productsData);
-    window.location.reload();
-}
