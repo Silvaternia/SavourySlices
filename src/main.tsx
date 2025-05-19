1493f09ed3a89d31362ff717cc35acd915e89683
Silvaire <105923294+Silvaternia@users.noreply.github.com>
2025-05-19 20:46:01 +0200

    Initial Tailwindcss setup + added a header ( Slow progression )

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Header from "./components/Header.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <Header />
+    </StrictMode>,
+);
