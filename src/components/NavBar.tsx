82d633ea3b1427c0440c758acc7d6a051b11cfc1
Silvaire <105923294+Silvaternia@users.noreply.github.com>
2025-05-24 11:37:30 +0200

    Alt: Used absolute ot position "logo (H2 )"

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,39 +6,35 @@ Make the menu icon trigger an left side canvas navigation bad
 
 function NavBar() {
     return (
-        <nav className="w-sm sm:w-lg md:w-3xl lg:w-5xl xl:w-7xl md:justify-between bg-dark-grey flex items-center text-surface-light px-4 h-16 transition-all duration-300 ease-in-out">
+        <nav className="w-full xl:w-7xl md:justify-between bg-dark-grey flex items-center text-surface-light px-6 h-24 relative transition-all duration-300 ease-in-out">
             <div className="flex items-center gap-4">
-                <div className="flex items-center">
+                <div className="flex items-center gap-4 grow">
                     <Menu size={40} />
-                    <ul className="hidden md:flex gap-2">
+                    <ul className="hidden lg:inline-flex gap-4">
                         <li>
                             <Link to="/">Home</Link>
                         </li>
                         <li>
-                            <Link to="/profiles">profiles</Link>
+                            <Link to="/profiles">About us</Link>
                         </li>
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Catagories</Link>
                         </li>
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Contact</Link>
                         </li>
                     </ul>
                 </div>
             </div>
-            <div className="flex grow md:grow-0 justify-center">
-                <h2 className="playfair-display-light text-3xl">SavourySlices</h2>
-            </div>
-
-            <div className="hidden md:flex gap-4">
-                <a href="#" className="flex gap-1 items-center px-6 py-2">
+            <h2 className="absolute left-1/2 transform -translate-x-1/2 playfair-display-light text-4xl md:text-5xl">
+                SavourySlices
+            </h2>
+            <div className="hidden lg:inline-flex gap-4">
+                <a href="#" className="flex gap-1 items-center px-8 py-2">
                     Order
                     <MoveDownRight size={18} strokeWidth={3} />
                 </a>
-                <button className="flex gap-1 items-center bg-primary px-6 py-2">
-                    login
-                    <MoveDownRight size={18} strokeWidth={3} />
-                </button>
+                <button className="flex gap-1 font-bold items-center bg-primary px-8 py-2.5">login</button>
             </div>
         </nav>
     );
